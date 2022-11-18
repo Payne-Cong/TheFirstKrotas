ad1a52795cb6f12677d536b0d305f88568c63ed4
Payne-Cong <lee.pl2020@outlook.com>
2022-11-18 23:06:25 +0800

    network proxy bash

---
+++ bashx/proxy.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+
+## wsl2 代理
+host_ip=$(cat /etc/resolv.conf | grep nameserver | awk '{ print $2 }')
+wsl_ip=$(hostname -I | awk '{print $1}')
+port=7892
+PROXY_SOCKS="socks5://${host_ip}:${port}"
+
+function display() {
+    echo "Host ip: ${host_ip}"
+    echo "WSL client ip: ${wsl_ip}"
+    echo "current PROXY: ${PROXY_SOCKS}"
+}
+
+function set_proxy() {
+    export http_proxy="${PROXY_SOCKS}"
+    export https_proxy="${PROXY_SOCKS}"
+    echo "env http/https proxy set."
+}
+
+function unset_proxy() {
+    unset http_proxy
+    unset https_proxy
+    echo "env proxy unset."
+}
+
+function set_git_proxy() {
+    git config --global http.proxy "${PROXY_SOCKS}"
+    git config --global https.proxy "${PROXY_SOCKS}"
+    echo "git config proxy set."
+}
+
+function unset_git_proxy() {
+    git config --global --unset http.proxy
+    git config --global --unset https.proxy
+    echo "git config proxy unset."
+}
+
+if [ "$1" = "display" ]; then
+    display
+elif [ "$1" = "set" ]; then
+    set_proxy
+elif [ "$1" = "unset" ]; then
+    unset_proxy
+elif [ "$1" = "setGit" ]; then
+    set_git_proxy
+elif [ "$1" = "unGit" ]; then
+    unset_git_proxy
+else
+    echo "please input [display, set, unset, setGit, unGit]."
+fi
